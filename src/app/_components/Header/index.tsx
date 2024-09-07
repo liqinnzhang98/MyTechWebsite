cbc304ba4bea7d89f53515385e77cef7ea12e18b
liqinzhang98 <liqizhang@student.unimelb.edu.au>
2024-09-07 11:57:00 +1000

    updated

---
+++ src/app/_components/Header/index.tsx
@@ -0,0 +1,25 @@
+{
+  /* eslint-disable @next/next/no-img-element */
+}
+
+import React from 'react'
+
+import type { Header } from '../../../payload/payload-types'
+import { fetchHeader } from '../../_api/fetchGlobals'
+import HeaderComponent from './HeaderComponent'
+
+export async function Header() {
+  let header: Header | null = null
+
+  try {
+    header = await fetchHeader()
+  } catch (error) {
+    console.log(error)
+  }
+
+  return (
+    <>
+      <HeaderComponent header={header} />
+    </>
+  )
+}
